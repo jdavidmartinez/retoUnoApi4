7ba56fa1ebe35c00428b8ff8d52b250c01fb7366
Juan David Martinez <jdavid.martinez@gmail.com>
2020-08-17 16:10:01 -0500

    Api lee de la url, y enruta en localhots:3000/'requerimiento' los requerimientos pleanteados, se puede mejorar

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 
-app.get('/user', function (req, res) {
+app.get('/femenino', function (req, res) {
     request({
 	    url: url,
 	    json: false
@@ -27,8 +27,90 @@ app.get('/user', function (req, res) {
         const mayorCuarenta = result.filter(val => val.edad > 40 )
 
         res.send(femenino) 
-         
-     }
+    }
+    
+   })
+});
+
+app.get('/masculino', function (req, res) {
+    request({
+	    url: url,
+	    json: false
+  },function (error, response, body) {
+        
+    let result = JSON.parse(body)
+    console.log(result[0])
+
+    if (!error && response.statusCode === 200) {
+        // Pintamos la respuesta JSON en navegador.
+        const masculino = result.filter(val => val.sexo === 'M') 
+       
+        res.send(masculino) 
+    }
+    
+   })
+});
+
+app.get('/veinte', function (req, res) {
+    request({
+	    url: url,
+	    json: false
+  },function (error, response, body) {
+        
+    let result = JSON.parse(body)
+    console.log(result[0])
+
+    if (!error && response.statusCode === 200) {
+        // Pintamos la respuesta JSON en navegador.
+        
+        const veinte = result.filter(val => val.edad <=20)
+        
+
+        res.send(veinte) 
+    }
+    
+   })
+});
+
+app.get('/cuarenta', function (req, res) {
+    request({
+	    url: url,
+	    json: false
+  },function (error, response, body) {
+        
+    let result = JSON.parse(body)
+    console.log(result[0])
+
+    if (!error && response.statusCode === 200) {
+        // Pintamos la respuesta JSON en navegador.
+        
+        const cuarenta = result.filter(val => val.edad > 20 && val.edad <=40)
+        
+        res.send(cuarenta) 
+    }
+    
+   })
+});
+
+app.get('/mayorcuarenta', function (req, res) {
+    request({
+	    url: url,
+	    json: false
+  },function (error, response, body) {
+        
+    let result = JSON.parse(body)
+    console.log(result[0])
+
+    if (!error && response.statusCode === 200) {
+        // Pintamos la respuesta JSON en navegador.
+        const masculino = result.filter(val => val.sexo === 'M') 
+        const femenino = result.filter(val => val.sexo === 'F')
+        const veinte = result.filter(val => val.edad <=20)
+        const cuarenta = result.filter(val => val.edad > 20 && val.edad <=40)
+        const mayorCuarenta = result.filter(val => val.edad > 40 )
+
+        res.send(mayorCuarenta) 
+    }
     
    })
 });
